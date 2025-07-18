1f63504b322d145a520e13b7874102e217fb92f7
Student <200598653@student.georgianc.on.ca>
2025-07-17 22:38:42 -0400

    Add assignment2 script

---
+++ repositoryname/makecontainers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Commented out the LXD check to allow running on existing containers
+# if lxc list | grep -q RUNNING; then
+#     echo "makecontainers.sh: This script is not designed to run with lxd, use a VM which has never had any containers yet"
+#     exit 1
+# fi
+
+# Your existing script content below...
+# (paste the rest of the script here without changes)
